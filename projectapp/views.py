3d01c6ed68eca0e196a567d43557fb0b0ee7b497
anusreepromod <anusreepi@gmail.com>
2021-12-04 12:07:08 +0530

    v6

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from projectapp.models import *
 
 # Create your views here.
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        login_obj = logins(email=email, password=password)
+        login_obj.save()
+        return render(request, 'login.html', {'msg': 'created'})
     return render(request, 'login.html')
